8856c5717f5e362eb310585e610732bda56a0b85
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-07-26 02:07:29 +0530

    Sample test

---
+++ JOIEnergy.Tests/SampleTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using JOIEnergy.Base.Entities;
+using JOIEnergy.Base.Enums;
+using Xunit;
+
+namespace JOIEnergy.Tests
+{
+    public class SampleTest
+    {
+
+        [Fact]
+        public void TestGetEnergySupplier() {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void TestGetBasePrice() {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void TestGetPeakTimePrice()
+        {
+            Assert.Equal(1, 1);
+        }
+
+    }
+}
