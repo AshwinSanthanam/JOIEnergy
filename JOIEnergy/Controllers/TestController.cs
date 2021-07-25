821ba157edae015a5140b6b2f494ae155eb651ab
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-07-26 01:27:59 +0530

    Test API Controller

---
+++ JOIEnergy/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JOIEnergy.Controllers
+{
+    [Route("test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Test");
+        }
+    }
+}
