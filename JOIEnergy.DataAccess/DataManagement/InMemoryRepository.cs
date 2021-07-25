b674e253e72d11f5b47bcfe9abf2e6130eebd820
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-07-25 06:33:55 +0530

    Created InMemory Repository

---
+++ JOIEnergy.DataAccess/DataManagement/InMemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using JOIEnergy.Base.DataManagement;
+using JOIEnergy.Base.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace JOIEnergy.DataAccess.DataManagement
+{
+    public class InMemoryRepository : IRepository
+    {
+        private readonly List<PricePlan> _pricePlans;
+        private Dictionary<string, MeterReadings> _meterAssociatedReadings;
+
+        public InMemoryRepository()
+        {
+            _pricePlans = new List<PricePlan>();
+            _meterAssociatedReadings = new Dictionary<string, MeterReadings>();
+        }
+
+        public MeterReadings InsertMeterReading(MeterReadings meterReadings)
+        {
+            _meterAssociatedReadings.Add(meterReadings.SmartMeterId, meterReadings);
+            return meterReadings;
+        }
+
+        public IEnumerable<PricePlan> PricePlans => throw new NotImplementedException();
+
+        public MeterReadings GetMeterReading(string meterId)
+        {
+            _meterAssociatedReadings.TryGetValue(meterId, out MeterReadings meterReadings);
+            return meterReadings;
+        }
+    }
+}
