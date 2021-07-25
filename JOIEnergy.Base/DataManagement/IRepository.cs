af8409ec96d275732b9b61bcc5bd527cc0f202b0
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-07-25 05:50:53 +0530

    Created IRepository

---
+++ JOIEnergy.Base/DataManagement/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using JOIEnergy.Base.Entities;
+using System.Collections.Generic;
+
+namespace JOIEnergy.Base.DataManagement
+{
+    public interface IRepository
+    {
+        MeterReadings GetMeterReading(string meterId);
+
+        IEnumerable<PricePlan> PricePlans { get; }
+    }
+}
