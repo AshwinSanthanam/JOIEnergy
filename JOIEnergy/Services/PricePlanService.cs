b2af0ebc32fe5fc1cc1ab65e9d7a6c9de0e0e7e9
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-07-25 20:42:53 +0530

    Price Plan Test

--- JOIEnergy/Services/PricePlanService.cs
+++ JOIEnergy/Services/PricePlanService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using JOIEnergy.Base.DataManagement;
 using JOIEnergy.Base.Entities;
@@ -8,7 +7,7 @@ namespace JOIEnergy.Services
 {
     public class PricePlanService : IPricePlanService
     {
-        private IMeterReadingService _meterReadingService;
+        private readonly IMeterReadingService _meterReadingService;
         private readonly IRepository _repository;
 
         public PricePlanService(IRepository repository, IMeterReadingService meterReadingService)
@@ -17,29 +16,7 @@ namespace JOIEnergy.Services
             _meterReadingService = meterReadingService;
         }
 
-        private decimal calculateAverageReading(IEnumerable<ElectricityReading> electricityReadings)
-        {
-            var newSummedReadings = electricityReadings.Select(readings => readings.Reading).Aggregate((reading, accumulator) => reading + accumulator);
-
-            return newSummedReadings / electricityReadings.Count();
-        }
-
-        private decimal calculateTimeElapsed(IEnumerable<ElectricityReading> electricityReadings)
-        {
-            var first = electricityReadings.Min(reading => reading.Time);
-            var last = electricityReadings.Max(reading => reading.Time);
-
-            return (decimal)(last - first).TotalHours;
-        }
-        private decimal calculateCost(IEnumerable<ElectricityReading> electricityReadings, PricePlan pricePlan)
-        {
-            var average = calculateAverageReading(electricityReadings);
-            var timeElapsed = calculateTimeElapsed(electricityReadings);
-            var averagedCost = average/timeElapsed;
-            return averagedCost * pricePlan.UnitRate;
-        }
-
-        public Dictionary<String, decimal> GetConsumptionCostOfElectricityReadingsForEachPricePlan(String smartMeterId)
+        public Dictionary<string, decimal> GetConsumptionCostOfElectricityReadingsForEachPricePlan(string smartMeterId)
         {
             IEnumerable<ElectricityReading> electricityReadings = _meterReadingService.GetReadings(smartMeterId);
 
@@ -47,7 +24,30 @@ namespace JOIEnergy.Services
             {
                 return new Dictionary<string, decimal>();
             }
-            return _repository.PricePlans.ToDictionary(plan => plan.EnergySupplier.ToString(), plan => calculateCost(electricityReadings, plan));
+            return _repository.PricePlans.ToDictionary(plan => plan.EnergySupplier.ToString(), plan => CalculateCost(electricityReadings, plan));
+        }
+
+        private decimal CalculateAverageReading(IEnumerable<ElectricityReading> electricityReadings)
+        {
+            var newSummedReadings = electricityReadings.Select(readings => readings.Reading).Aggregate((reading, accumulator) => reading + accumulator);
+
+            return newSummedReadings / electricityReadings.Count();
+        }
+
+        private decimal CalculateTimeElapsed(IEnumerable<ElectricityReading> electricityReadings)
+        {
+            var first = electricityReadings.Min(reading => reading.Time);
+            var last = electricityReadings.Max(reading => reading.Time);
+
+            return (decimal)(last - first).TotalHours;
+        }
+
+        private decimal CalculateCost(IEnumerable<ElectricityReading> electricityReadings, PricePlan pricePlan)
+        {
+            var average = CalculateAverageReading(electricityReadings);
+            var timeElapsed = CalculateTimeElapsed(electricityReadings);
+            var averagedCost = average/timeElapsed;
+            return averagedCost * pricePlan.UnitRate;
         }
     }
 }
